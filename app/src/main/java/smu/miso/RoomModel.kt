82282200267e1a6e0142be6b15570c36b11bfbcb
kkangjee <wlgns337@gmail.com>
2020-08-05 13:39:01 +0900

    slack test

---
+++ app/src/main/java/smu/miso/RoomModel.kt
@@ -0,0 +1,7 @@
+package smu.miso
+
+class RoomModel {
+    private var message: String? = null
+    private var users: String? = null
+
+}
---
+++ app/src/main/java/smu/miso/UserModel.kt
@@ -0,0 +1,24 @@
+package smu.miso
+
+class UserModel() {
+    private var uid: String? = null
+    private var token: String? = null
+    private var userid: String? = null
+    private var usermsg: String? = null
+    private var usernm: String? = null
+
+    private var email: String = ""
+    private var emailVerified: Boolean = false
+    private var department: String = ""
+    private var tags: ArrayList<String>? = null
+
+    //회원가입 시 필요한 정보
+    constructor (uid: String, email: String?, emailVerified:Boolean, department: String?, tags : ArrayList<String>?) : this()
+    {
+        this.uid = uid
+        this.email = email.toString()
+        this.emailVerified = emailVerified
+        this.department = department.toString()
+        this.tags = tags
+    }
+}
