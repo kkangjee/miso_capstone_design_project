b0a55a1b0d69d024be2d7bfe797929b7ac8314b1
kkangjee <wlgns337@gmail.com>
2020-08-28 12:26:44 +0900

    사용자 구분 구현

--- app/src/main/java/smu/miso/ui/randomchat/RandomChatFragment.kt
+++ app/src/main/java/smu/miso/ui/randomchat/RandomChatFragment.kt
@@ -1,26 +1,40 @@
 package smu.miso.ui.randomchat
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_randomchat.*
+import smu.miso.Chat.ChatFragment
 import smu.miso.R
+
 
 class RandomChatFragment : Fragment() {
     //전역 변수 설정 공간
     private lateinit var randomchatViewModel: RandomChatViewModel
+    private val database = FirebaseDatabase.getInstance()
+    private val userRef = database.reference
+    private val uid = FirebaseAuth.getInstance().currentUser?.uid.toString()
+
     //기본 구성 함수(Don't touch)
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {//여기까지
         randomchatViewModel =
-                ViewModelProviders.of(this).get(RandomChatViewModel::class.java)
+            ViewModelProviders.of(this).get(RandomChatViewModel::class.java)
         //어떤 화면을 container(프레그먼트가 보여질 화면)에 보여질 것인지 설정
         val root = inflater.inflate(R.layout.fragment_randomchat, container, false)
         val textView: TextView = root.findViewById(R.id.text_randomchat)
@@ -28,10 +42,78 @@ class RandomChatFragment : Fragment() {
             textView.text = it
         })
         return root
-}
+    }
+
     //버튼 등의 리스너를 달 때에는 onViewCreated안에 작성
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        var department: String = ""
+        var deptKey: String = ""
+        //과끼리 랜덤채팅
+        button.setOnClickListener {
+            userRef.child("users").child(uid).addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    department = snapshot.child("department").value.toString()
+                    Log.d("과 끼리 학과 가져오기", department)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e("학과 가져오기 실패", "두둥탁")
+                }
+            })
+
+
+            userRef.child("deptMap").child(department).addListenerForSingleValueEvent(object :
+                ValueEventListener {
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    //Log.d("학과 가져오기", department)
+
+                    for (singleSnapshot in snapshot.children) {
+                        //Log.d("스냅샷",singleSnapshot.key.toString())
+
+                        //내 학과와 같은 상황일 때 반복문 종료
+                        if (singleSnapshot.key.toString() == department) {
+                            Log.d("기존 uid 가져오기", deptKey)
+                            deptKey = snapshot.child(department).value.toString()
+                            break
+                        }
+
+                    }
+
+
+                    if (deptKey == "") {
+                        //TODO: 아무도 대기 상태가 아닐 경우 대기상태로 변경하고 value값을 넣어준다.
+                        userRef.child("deptMap").child(department)
+                            .setValue(uid)
+                    } else {
+                        //TODO: 이미 uid 값이 있는 것을 확인하여 손 잡고 채팅방을 나간다. 그리고 value값을 비운다.
+                        Log.d("deptKey이미 존재", "확인")
+                        userRef.child("deptMap").child(department)
+                            .setValue("")
+                    }
+
+                    fragmentManager?.beginTransaction()?.apply {
+                        replace(R.id.nav_host_fragment, ChatFragment())
+                        setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        commit()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+        }
+        //전체과 랜덤채팅
+        button2.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.nav_host_fragment, ChatFragment())
+                setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                commit()
+            }
+        }
     }
     //사용자 설정 함수나 override함수들(onBackPressed같은거)은 여기에 작성
 }
