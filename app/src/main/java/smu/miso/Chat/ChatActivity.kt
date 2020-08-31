bb7338baf65f7e3c38a4a206aada4444b1cc2218
문승재 <gaeddongie13@gmail.com>
2020-08-31 17:13:37 +0900

    chat room 내부 toolbar menu 구현

--- app/src/main/java/smu/miso/Chat/ChatActivity.kt
+++ app/src/main/java/smu/miso/Chat/ChatActivity.kt
@@ -1,33 +1,46 @@
 package smu.miso.Chat
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
-import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import kotlinx.android.synthetic.main.activity_chat.*
 import smu.miso.R
-import smu.miso.SplashActivity
 
 
 class ChatActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
-        supportActionBar?.hide()
+        //supportActionBar?.hide()
 
-        val splashIntent = Intent(this, SplashActivity::class.java)
-        startActivity(splashIntent)
+        supportActionBar!!.setDisplayShowTitleEnabled(false) // 타이틀 안보이게 하기
 
         // chatting area
         supportFragmentManager.beginTransaction()
             .replace(R.id.mainFragment, ChatFragment())
             .commit()
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.item_setting_chatroom, menu)
+        return true
+    }
 
-
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar items
+        // 채팅방 메뉴 항목 선택
+        //TODO : 채팅 방 나가기, 상대방 신고 기능 추가.
+        when (item.itemId){
+            R.id.action_exitroom -> {
+                Toast.makeText(this, "채팅 방을 폭파", Toast.LENGTH_SHORT).show()
+            }
+            R.id.action_opponent_alert -> {
+                Toast.makeText(this, "상대방을 신고",Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
+
+
